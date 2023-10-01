c9adab781ee4ed14588d518d302187a6fb77fd2c
AleksandraMaiurova <asmaiurova@gmail.com>
2023-10-01 12:14:04 +0300

    added new file with demoqa for Selene

--- conftest.py
+++ conftest.py
@@ -4,7 +4,9 @@ from selene.support.shared import browser
 
 @pytest.fixture
 def size(scope="session"):
-    browser.config.window_height = 1080
-    browser.config.window_width = 1920
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.timeout = 4.0
+    browser.config.window_height = 800
+    browser.config.window_width = 400
     yield
     browser.quit()
